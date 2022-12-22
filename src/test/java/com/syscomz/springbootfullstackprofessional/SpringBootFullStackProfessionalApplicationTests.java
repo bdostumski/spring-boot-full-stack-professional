3193a863dbc61c1ab285f1a9233f5d6cb760b53c
Borislav Dostumski <b.dostumski@gmail.com>
2022-12-22 12:39:49 +0200

    try pass test

--- src/test/java/com/syscomz/springbootfullstackprofessional/SpringBootFullStackProfessionalApplicationTests.java
+++ src/test/java/com/syscomz/springbootfullstackprofessional/SpringBootFullStackProfessionalApplicationTests.java
@@ -1,6 +1,5 @@
 package com.syscomz.springbootfullstackprofessional;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,8 +8,8 @@ class SpringBootFullStackProfessionalApplicationTests {
 
 	@Test
 	void contextLoads() {
-		Assertions.fail("ERROR MESSAGE");
-		Assertions.assertTrue(Boolean.FALSE);
+//		Assertions.fail("ERROR MESSAGE");
+//		Assertions.assertTrue(Boolean.FALSE);
 	}
 
 }
