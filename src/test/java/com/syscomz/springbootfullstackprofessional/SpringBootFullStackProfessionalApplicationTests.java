a215ff286b27941f805e40c4f10493c18daf750d
Borislav Dostumski <b.dostumski@gmail.com>
2022-12-22 14:09:34 +0200

    pass test

--- src/test/java/com/syscomz/springbootfullstackprofessional/SpringBootFullStackProfessionalApplicationTests.java
+++ src/test/java/com/syscomz/springbootfullstackprofessional/SpringBootFullStackProfessionalApplicationTests.java
@@ -9,8 +9,8 @@ class SpringBootFullStackProfessionalApplicationTests {
 
 	@Test
 	void contextLoads() {
-		Assertions.fail("ERROR MESSAGE");
-		Assertions.assertTrue(Boolean.FALSE);
+//		Assertions.fail("ERROR MESSAGE");
+//		Assertions.assertTrue(Boolean.FALSE);
 	}
 
 }
