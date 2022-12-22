970728aef1378bf231b774a7bb4b6577ab7bd2bc
Borislav Dostumski <b.dostumski@gmail.com>
2022-12-22 13:00:17 +0200

    fix tests

--- src/test/java/com/syscomz/springbootfullstackprofessional/SpringBootFullStackProfessionalApplicationTests.java
+++ src/test/java/com/syscomz/springbootfullstackprofessional/SpringBootFullStackProfessionalApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = SpringBootFullStackProfessionalApplicationTests.class)
 class SpringBootFullStackProfessionalApplicationTests {
 
 	@Test
